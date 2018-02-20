ec4c8bf0dbc99edb2680bd40af3ab06d117a21ae
zhongxuchen <zhongxuchen@gmail.com>
2018-02-20 22:04:32 +0800

    将showcase放入tags下面

---
+++ tags/sqltoy-showcase/readme.txt
@@ -0,0 +1,5 @@
+使用说明：
+1、请参照sagacity-sqltoy/docs 中的说明文档安装sqltoy的schema
+2、请安装mysql数据库,参见src/main/resources/system.properties 配置数据库
+3、请通过sqltoy-showcase/docs 下面的表结构创建表信息
+4、点击src/test/java/sqltoy/showcase/service 目录下面的测试类进行功能验证
---
+++ tags/sqltoy-showcase/src/main/java/sqltoy/showcase/system/dao/StaffInfoDao.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package sqltoy.showcase.system.dao;
+
+import java.util.List;
+
+import org.sagacity.sqltoy.model.PaginationModel;
+import org.sagacity.sqltoy.support.BaseDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import sqltoy.showcase.system.vo.Goods;
+import sqltoy.showcase.system.vo.GoodsParam;
+import sqltoy.showcase.system.vo.OrganInfoVO;
+import sqltoy.showcase.system.vo.StaffInfoVO;
+
+/**
+ * @project sqltoy-showcase
+ * @description
+ *              <p>
+ *              员工信息操作
+ *              </p>
+ * @author chenrenfei <a href="mailto:zhongxuchen@gmail.com">联系作者</a>
+ * @version id:StaffInfoDao.java,Revision:v1.0,Date:2017年11月8日
+ */
+@Repository("staffInfoDao")
+public class StaffInfoDao extends BaseDaoSupport {
+	/**
+	 * 分页查询员工信息
+	 * 
+	 * @param pageModel
+	 * @param staffInfoVO
+	 * @return
+	 * @throws Exception
+	 */
+	public PaginationModel findPage(PaginationModel pageModel, StaffInfoVO staffInfoVO) throws Exception {
+		return page().pageModel(pageModel).sql("sys_findStaff").entity(staffInfoVO).submit();
+	}
+
+	/**
+	 * 保存员工信息
+	 * 
+	 * @param staffInfos
+	 * @throws Exception
+	 */
+	public void saveOrUpdateStaff(List<StaffInfoVO> staffInfos) throws Exception {
+		save().saveMode(IGNORE).many(staffInfos);
+	}
+
+	public List findES(OrganInfoVO organInfoVO) throws Exception {
+		return elastic().sql("sys_elastic_test_json").entity(organInfoVO).find();
+	}
+
+	public PaginationModel findESPage(PaginationModel pageModel, OrganInfoVO organInfoVO) throws Exception {
+		return elastic().sql("sys_elastic_test_json").entity(organInfoVO).findPage(pageModel);
+	}
+
+	public List findESBySql(OrganInfoVO organInfoVO) throws Exception {
+		return elastic().sql("sys_elastic_test").entity(organInfoVO).find();
+	}
+
+	public PaginationModel findESPageBySql(PaginationModel pageModel, GoodsParam goodsParam) throws Exception {
+		return elastic().sql("query_goods").entity(goodsParam).resultType(Goods.class).findPage(pageModel);
+	}
+
+	public List findESAggs(PaginationModel pageModel, OrganInfoVO organInfoVO) throws Exception {
+		return elastic().sql("sys_elastic_test_aggs").entity(organInfoVO).find();
+	}
+}
