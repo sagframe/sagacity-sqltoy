2cdcfc9fe09525a87d9440d90392fa1675cb2b13
chenrenfei <102309713@qq.com>
2019-03-25 15:23:43 +0800

    完善基于springsecurity的默认字段赋值逻辑

--- trunk/sagacity-sqltoy-4.3/src/main/java/org/sagacity/sqltoy/plugin/IUnifyFieldsHandler.java
+++ trunk/sagacity-sqltoy-4.3/src/main/java/org/sagacity/sqltoy/plugin/IUnifyFieldsHandler.java
@@ -13,18 +13,39 @@ import java.util.Map;
  */
 // 注:sqltoy会自动判断是否有相关属性,属性不存在则不会进行操作
 // 针对saveOrUpdate操作,sqltoy则分别调用创建和修改的赋值,同时避免修改时冲掉创建人和创建时间信息
-public interface IUnifyFieldsHandler {
+public abstract class IUnifyFieldsHandler {
+	/**
+	 * 强制对创建时间、修改时间进行修改
+	 */
+	private boolean forceUpdateTime = false;
+
 	/**
 	 * 返回创建记录时需要修改的字段和对应的值
 	 * 
 	 * @return
 	 */
-	public Map<String, Object> createUnifyFields();
+	public abstract Map<String, Object> createUnifyFields();
 
 	/**
 	 * 返回修改记录时相关字段的名称和值
 	 * 
 	 * @return
 	 */
-	public Map<String, Object> updateUnifyFields();
+	public abstract Map<String, Object> updateUnifyFields();
+
+	/**
+	 * @return the forceUpdateTime
+	 */
+	public boolean isForceUpdateTime() {
+		return forceUpdateTime;
+	}
+
+	/**
+	 * @param forceUpdateTime
+	 *            the forceUpdateTime to set
+	 */
+	public void setForceUpdateTime(boolean forceUpdateTime) {
+		this.forceUpdateTime = forceUpdateTime;
+	}
+
 }
--- trunk/sagacity-sqltoy-4.3/src/main/java/org/sagacity/sqltoy/plugin/unifyfields/SpringSecurityUnifyFieldsHandler.java
+++ trunk/sagacity-sqltoy-4.3/src/main/java/org/sagacity/sqltoy/plugin/unifyfields/SpringSecurityUnifyFieldsHandler.java
@@ -10,6 +10,9 @@ import java.util.Map;
 
 import org.sagacity.sqltoy.plugin.IUnifyFieldsHandler;
 import org.sagacity.sqltoy.utils.DateUtil;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  * @project sagacity-sqltoy4.0
@@ -17,7 +20,7 @@ import org.sagacity.sqltoy.utils.DateUtil;
  * @author chenrenfei <a href="mailto:zhongxuchen@gmail.com">联系作者</a>
  * @version id:SpringSecurityUnifyFieldsHandler.java,Revision:v1.0,Date:2018年1月17日
  */
-public class SpringSecurityUnifyFieldsHandler implements IUnifyFieldsHandler {
+public class SpringSecurityUnifyFieldsHandler extends IUnifyFieldsHandler {
 	// 注:sqltoy会自动判断是否有相关属性,属性不存在则不会进行操作
 	// 针对saveOrUpdate操作,sqltoy则分别调用创建和修改的赋值,同时避免修改时冲掉创建人和创建时间信息
 	/*
@@ -30,7 +33,8 @@ public class SpringSecurityUnifyFieldsHandler implements IUnifyFieldsHandler {
 		HashMap<String, Object> keyValueMap = new HashMap<String, Object>();
 		Date nowDate = DateUtil.getNowTime();
 		Timestamp nowTime = DateUtil.getTimestamp(null);
-		String userId = "";
+		// 获取用户信息
+		String userId = getCurrentUserName();
 		keyValueMap.put("createBy", userId);
 		keyValueMap.put("createDate", nowDate);
 		keyValueMap.put("createTime", nowTime);
@@ -50,11 +54,24 @@ public class SpringSecurityUnifyFieldsHandler implements IUnifyFieldsHandler {
 		HashMap<String, Object> keyValueMap = new HashMap<String, Object>();
 		Date nowDate = DateUtil.getNowTime();
 		Timestamp nowTime = DateUtil.getTimestamp(null);
-		String userId = "";
+		// 获取用户信息
+		String userId = getCurrentUserName();
 		keyValueMap.put("updateBy", userId);
 		keyValueMap.put("updateDate", nowDate);
 		keyValueMap.put("updateTime", nowTime);
 		return keyValueMap;
+	}
+
+	private String getCurrentUserName() {
+		SecurityContext context = SecurityContextHolder.getContext();
+		Authentication authentication = null;
+		if (context != null) {
+			authentication = context.getAuthentication();
+		}
+		if (authentication != null && authentication.getPrincipal() != null) {
+			return authentication.getName();
+		}
+		return "";
 	}
 
 }
