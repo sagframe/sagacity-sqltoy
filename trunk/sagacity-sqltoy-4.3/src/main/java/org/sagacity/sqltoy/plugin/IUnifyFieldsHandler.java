7abee17c270b777a39ed1173c8d211d5fce4ae58
chenrenfei <102309713@qq.com>
2019-04-29 19:37:39 +0800

    Update IUnifyFieldsHandler.java

--- trunk/sagacity-sqltoy-4.3/src/main/java/org/sagacity/sqltoy/plugin/IUnifyFieldsHandler.java
+++ trunk/sagacity-sqltoy-4.3/src/main/java/org/sagacity/sqltoy/plugin/IUnifyFieldsHandler.java
@@ -9,16 +9,9 @@ import java.util.Map;
  * @project sagacity-sqltoy4.0
  * @description 统一字段赋值处理
  * @author chenrenfei <a href="mailto:zhongxuchen@gmail.com">联系作者</a>
- * @version id:AbstractUnifyFieldsHandler.java,Revision:v1.0,Date:2018年1月17日
+ * @version id:IUnifyFieldsHandler.java,Revision:v1.0,Date:2018年1月17日
  */
-// 注:sqltoy会自动判断是否有相关属性,属性不存在则不会进行操作
-// 针对saveOrUpdate操作,sqltoy则分别调用创建和修改的赋值,同时避免修改时冲掉创建人和创建时间信息
 public interface IUnifyFieldsHandler {
-	/**
-	 * 强制对创建时间、修改时间进行修改
-	 */
-	//private boolean forceUpdateTime = false;
-
 	/**
 	 * 返回创建记录时需要修改的字段和对应的值
 	 * 
@@ -32,20 +25,5 @@ public interface IUnifyFieldsHandler {
 	 * @return
 	 */
 	public Map<String, Object> updateUnifyFields();
-
-//	/**
-//	 * @return the forceUpdateTime
-//	 */
-//	public boolean isForceUpdateTime() {
-//		return forceUpdateTime;
-//	}
-//
-//	/**
-//	 * @param forceUpdateTime
-//	 *            the forceUpdateTime to set
-//	 */
-//	public void setForceUpdateTime(boolean forceUpdateTime) {
-//		this.forceUpdateTime = forceUpdateTime;
-//	}
 
 }
