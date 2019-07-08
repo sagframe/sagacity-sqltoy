49924dbccaee8fed585b69287391d27be916400b
chenrenfei <102309713@qq.com>
2019-07-08 13:32:54 +0800

    Update SqlToyLazyDaoImpl.java

--- trunk/sagacity-sqltoy-4.3/src/main/java/org/sagacity/sqltoy/dao/impl/SqlToyLazyDaoImpl.java
+++ trunk/sagacity-sqltoy-4.3/src/main/java/org/sagacity/sqltoy/dao/impl/SqlToyLazyDaoImpl.java
@@ -6,7 +6,6 @@ package org.sagacity.sqltoy.dao.impl;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.sql.DataSource;
