5e7058cf4e7b714892d53d862c53223217cad3d0
chenrenfei <102309713@qq.com>
2019-07-11 13:53:42 +0800

    重新开始编写showcase

---
+++ trunk/sqltoy-showcase/src/main/java/org/sagacity/sqltoy/plugins/ShowCaseUnifyFieldsHandler.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package org.sagacity.sqltoy.plugins;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.sagacity.sqltoy.plugin.IUnifyFieldsHandler;
+import org.sagacity.sqltoy.utils.DateUtil;
+
+/**
+ * @project sqltoy-showcase
+ * @description 统一字段赋值范例
+ * @author chenrenfei <a href="mailto:zhongxuchen@gmail.com">联系作者</a>
+ * @version id:ShowCaseUnifyFieldsHandler.java,Revision:v1.0,Date:2018年1月18日
+ */
+public class ShowCaseUnifyFieldsHandler implements IUnifyFieldsHandler {
+	private String defaultUserName = "system-auto";
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sagacity.sqltoy.plugin.IUnifyFieldsHandler#createUnifyFields()
+	 */
+	@Override
+	public Map<String, Object> createUnifyFields() {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		Date nowDate = DateUtil.getNowTime();
+		Timestamp nowTime = DateUtil.getTimestamp(null);
+		// 获取用户信息
+		String userId = getUserId();
+		map.put("createBy", userId);
+		map.put("createDate", nowDate);
+		map.put("createTime", nowTime);
+		map.put("updateBy", userId);
+		map.put("updateDate", nowDate);
+		map.put("updateTime", nowTime);
+		map.put("systemTime", nowTime);
+		// enabled 是否启用状态
+		map.put("enabled", 1);
+		return map;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sagacity.sqltoy.plugin.IUnifyFieldsHandler#updateUnifyFields()
+	 */
+	@Override
+	public Map<String, Object> updateUnifyFields() {
+		Timestamp timestamp = DateUtil.getTimestamp(null);
+		Map<String, Object> map = new HashMap<String, Object>();
+		// 获取用户信息
+		map.put("updateBy", getUserId());
+		map.put("updateTime", timestamp);
+		map.put("updateDate", timestamp);
+		map.put("systemTime", timestamp);
+		return map;
+	}
+
+	/**
+	 * @todo 获取当前用户Id信息
+	 * @return
+	 */
+	private String getUserId() {
+		// 通过SpringSecurity 获取当前用户
+		// return (SpringSecurityUtils.getCurrentUser() != null) ?
+		// SpringSecurityUtils.getCurrentUser().getId() : defaultUserName;
+		return defaultUserName;
+	}
+
+	/**
+	 * @return the defaultUserName
+	 */
+	public String getDefaultUserName() {
+		return defaultUserName;
+	}
+
+	/**
+	 * @param defaultUserName
+	 *            the defaultUserName to set
+	 */
+	public void setDefaultUserName(String defaultUserName) {
+		this.defaultUserName = defaultUserName;
+	}
+
+}
