e921ccb46c0107326d06f25bff480c0a4735e784
zhongxuchen <zhongxuchen@gmail.com>
2019-07-15 23:53:12 +0800

    优化代码

--- trunk/sqltoy-showcase/src/test/java/org/sagacity/sqltoy/showcase/CrudCaseServiceTest.java
+++ trunk/sqltoy-showcase/src/test/java/org/sagacity/sqltoy/showcase/CrudCaseServiceTest.java
@@ -6,17 +6,17 @@ package org.sagacity.sqltoy.showcase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.sagacity.sqltoy.SqlToyApplication;
-import org.sagacity.sqltoy.showcase.service.CrudCaseService;
+import org.sagacity.sqltoy.service.SqlToyCRUDService;
 import org.sagacity.sqltoy.showcase.vo.StaffInfoVO;
-import org.sagacity.sqltoy.utils.ShowCaseUtils;
 import org.sagacity.sqltoy.utils.DateUtil;
+import org.sagacity.sqltoy.utils.ShowCaseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @project sqltoy-showcase
- * @description 请在此说明类的功能
+ * @description 普通的CRUD操作演示，无需额外写service方法，由sqltoy提供SqlToyCRUDServcie提供默认的操作即可
  * @author chenrenfei <a href="mailto:zhongxuchen@gmail.com">联系作者</a>
  * @version id:CrudCaseServiceTest.java,Revision:v1.0,Date:2019年7月12日
  */
@@ -24,8 +24,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = SqlToyApplication.class)
 public class CrudCaseServiceTest {
 	@Autowired
-	private CrudCaseService crudCaseService;
+	private SqlToyCRUDService sqlToyCRUDService;
 
+	/**
+	 * 创建一条员工记录
+	 */
 	@Test
 	public void saveStaffInfo() {
 		StaffInfoVO staffInfo = new StaffInfoVO();
@@ -39,9 +42,12 @@ public class CrudCaseServiceTest {
 		staffInfo.setOrganId("C0001");
 		staffInfo.setPhoto(ShowCaseUtils.getBytes(ShowCaseUtils.getFileInputStream("classpath:/mock/staff_photo.jpg")));
 		staffInfo.setCountry("86");
-		crudCaseService.saveStaffInfo(staffInfo);
+		sqlToyCRUDService.save(staffInfo);
 	}
 
+	/**
+	 * 修改员工记录信息
+	 */
 	@Test
 	public void updateStaffInfo() {
 		StaffInfoVO staffInfo = new StaffInfoVO();
@@ -55,6 +61,49 @@ public class CrudCaseServiceTest {
 		staffInfo.setOrganId("C0001");
 		staffInfo.setPhoto(ShowCaseUtils.getBytes(ShowCaseUtils.getFileInputStream("classpath:/mock/staff_photo.jpg")));
 		staffInfo.setCountry("86");
-		crudCaseService.saveStaffInfo(staffInfo);
+		sqlToyCRUDService.update(staffInfo);
+	}
+
+	/**
+	 * 对员工信息特定字段进行强制修改
+	 */
+	@Test
+	public void forceUpdate() {
+
+	}
+
+	@Test
+	public void saveOrUpdate() {
+
+	}
+
+	@Test
+	public void saveOrUpdateAll() {
+
+	}
+
+	@Test
+	public void saveAll() {
+
+	}
+
+	@Test
+	public void delete() {
+
+	}
+
+	@Test
+	public void deleteAll() {
+
+	}
+
+	@Test
+	public void load() {
+
+	}
+
+	@Test
+	public void loadAll() {
+
 	}
 }
---
+++ trunk/sqltoy-showcase/src/test/java/org/sagacity/sqltoy/showcase/QueryCaseTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.sagacity.sqltoy.showcase;
+
+import org.junit.runner.RunWith;
+import org.sagacity.sqltoy.SqlToyApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @project sqltoy-boot-showcase
+ * @description 演示sql查询
+ * @author chenrenfei <a href="mailto:zhongxuchen@gmail.com">联系作者</a>
+ * @version id:QueryCaseTest.java,Revision:v1.0,Date:2019年7月15日
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SqlToyApplication.class)
+public class QueryCaseTest {
+
+}
