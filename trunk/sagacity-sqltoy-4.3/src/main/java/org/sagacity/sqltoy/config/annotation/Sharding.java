f9a3785523ba21f4030caf32dad7d38ae89eeb45
chenrenfei <102309713@qq.com>
2019-07-31 13:13:34 +0800

    Update Sharding.java

--- trunk/sagacity-sqltoy-4.3/src/main/java/org/sagacity/sqltoy/config/annotation/Sharding.java
+++ trunk/sagacity-sqltoy-4.3/src/main/java/org/sagacity/sqltoy/config/annotation/Sharding.java
@@ -18,17 +18,19 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 public @interface Sharding {
 
-	// 最大等待秒数,0表示不限制
+	// 最大等待秒数,小于等于0表示不限制
 	int maxWaitSeconds() default -1;
 
 	// 最大并行数,默认为10,用于批量操作时可以同时向多个数据源同时执行
 	// 实际会按照数据源的多少进行并行,同时考虑CPU的核心数量
 	int maxConcurrents() default 10;
 
+	// 分库策略
 	Strategy db() default @Strategy();
 
+	// 分表策略
 	Strategy table() default @Strategy();
 
-	//在增加、修改、删除操作是个别节点发生异常，事务回滚策略是整体还是局部
+	// 在增加、修改、删除操作是个别节点发生异常，事务回滚策略是整体还是局部(默认为全局)
 	boolean is_global_rollback() default true;
 }
