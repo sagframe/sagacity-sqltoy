c8a5406d6d34be9b98b6179478371b346c0bc200
chenrenfei <102309713@qq.com>
2019-08-08 17:34:23 +0800

    Update ElasticCaseServiceTest.java

--- trunk/sqltoy-showcase/src/test/java/com/sagframe/sqltoy/showcase/ElasticCaseServiceTest.java
+++ trunk/sqltoy-showcase/src/test/java/com/sagframe/sqltoy/showcase/ElasticCaseServiceTest.java
@@ -3,7 +3,10 @@
  */
 package com.sagframe.sqltoy.showcase;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.sagacity.sqltoy.dao.SqlToyLazyDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,12 +14,29 @@ import com.sagframe.sqltoy.SqlToyApplication;
 
 /**
  * @project sqltoy-showcase
- * @description 请在此说明类的功能
+ * @description 演示elasticsearch5.x+ 版本的使用
  * @author chenrenfei <a href="mailto:zhongxuchen@gmail.com">联系作者</a>
  * @version id:ElasticCaseServiceTest.java,Revision:v1.0,Date:2019年7月12日
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SqlToyApplication.class)
 public class ElasticCaseServiceTest {
+	@Autowired
+	private SqlToyLazyDao sqlToyLazyDao;
 
+	/**
+	 * 演示普通的查询
+	 */
+	@Test
+	public void testSearch() {
+
+	}
+
+	/**
+	 * 演示分页查询
+	 */
+	@Test
+	public void testFindPage() {
+
+	}
 }
