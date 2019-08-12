ef9aa29556cb8183acdacf77c5b1d23edc06a1c8
chenrenfei <102309713@qq.com>
2019-08-12 09:23:55 +0800

    Update DateUtil.java

--- trunk/sagacity-sqltoy-4.3/src/main/java/org/sagacity/sqltoy/utils/DateUtil.java
+++ trunk/sagacity-sqltoy-4.3/src/main/java/org/sagacity/sqltoy/utils/DateUtil.java
@@ -553,18 +553,26 @@ public class DateUtil {
 	}
 
 	public static Date asDate(LocalDate localDate) {
+		if (localDate == null)
+			return null;
 		return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
 	}
 
 	public static Date asDate(LocalDateTime localDateTime) {
+		if (localDateTime == null)
+			return null;
 		return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
 	}
 
 	public static LocalDate asLocalDate(Date date) {
+		if (date == null)
+			return null;
 		return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
 	}
 
 	public static LocalDateTime asLocalDateTime(Date date) {
+		if (date == null)
+			return null;
 		return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
 	}
 }
