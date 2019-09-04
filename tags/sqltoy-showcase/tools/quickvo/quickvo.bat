5bbb69ca27fe95a73414385fba9d320c95305582
zhongxuchen <zhongxuchen@gmail.com>
2019-09-04 17:32:46 +0800

    删除tag旧的showcase

--- tags/sqltoy-showcase/tools/quickvo/quickvo.bat
+++
@@ -1,4 +0,0 @@
-﻿@echo ============== start generate vo and abstractvo file ==================
-java -jar sagacity-quickvo.jar quickvo.xml
-@echo ============== end generate vo and abstractvo file ====================
-cmd
