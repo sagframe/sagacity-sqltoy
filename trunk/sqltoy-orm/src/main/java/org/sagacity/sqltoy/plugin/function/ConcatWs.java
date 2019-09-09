fed03e429933407cd50922faca00696090409053
chenrenfei <102309713@qq.com>
2019-09-09 11:12:41 +0800

    Create ConcatWs.java

---
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/plugin/function/ConcatWs.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package org.sagacity.sqltoy.plugin.function;
+
+import org.sagacity.sqltoy.plugin.IFunction;
+import org.sagacity.sqltoy.utils.DataSourceUtils.DBType;
+
+/**
+ * @project sqltoy-orm
+ * @description 针对mysql数据库字符连接函数concat在其它数据库中的函数转换
+ * @author renfei.chen <a href="mailto:zhongxuchen@gmail.com">联系作者</a>
+ * @version id:Concat.java,Revision:v1.0,Date:2013-3-21
+ */
+public class ConcatWs extends IFunction {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sagacity.sqltoy.config.function.IFunction#dialects()
+	 */
+	@Override
+	public String dialects() {
+		return "oracle";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sagacity.sqltoy.config.function.IFunction#regex()
+	 */
+	@Override
+	public String regex() {
+		return "(?i)\\Wconcat_ws\\(";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sagacity.sqltoy.config.function.IFunction#wrap(int,
+	 * java.lang.String[])
+	 */
+	@Override
+	public String wrap(int dialect, String functionName, boolean hasArgs, String... args) {
+		// 只针对oracle数据库,其他数据库原样返回
+		if (dialect == DBType.ORACLE || dialect == DBType.ORACLE12) {
+			StringBuilder result = new StringBuilder();
+			String split = args[0];
+			for (int i = 1; i < args.length; i++) {
+				if (i > 1) {
+					result.append("||").append(split).append("||");
+				}
+				result.append(args[i]);
+			}
+			return result.toString();
+		}
+		return null;
+	}
+
+}
