4a0ec4bd7ccd7a4b7edd8dabf5043a89495be7b6
zhongxuchen <zhongxuchen@gmail.com>
2019-09-25 16:45:03 +0800

    quickvo全部按数据库方言来提供
    优化配置

--- trunk/sqltoy-showcase/tools/quickvo/quickvo.bat
+++ trunk/sqltoy-showcase/tools/quickvo/quickvo.bat
@@ -1,4 +1,4 @@
 ﻿@echo ============== start generate vo and abstractvo file ==================
-java -jar sqltoy-quickvo-4.6.jar quickvo.xml
+java -jar sqltoy-quickvo-mysql.jar quickvo.xml
 @echo ============== end generate vo and abstractvo file ====================
 cmd
