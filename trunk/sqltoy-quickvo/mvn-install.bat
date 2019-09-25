ab43125117ec29124667fc45ee1ae5582246cea8
zhongxuchen <zhongxuchen@gmail.com>
2019-09-25 16:19:47 +0800

    根据数据库分别打包
    根据数据库分别打包

--- trunk/sqltoy-quickvo/mvn-install.bat
+++ trunk/sqltoy-quickvo/mvn-install.bat
@@ -1,8 +1,6 @@
 @echo off
 set list=mysql db2 mssql oracle postgre
 (for %%a in (%list%) do (
-   mvn clean deploy -Dmaven.test.skip=true -f pom-%%a.xml
+   mvn clean package install -Dmaven.test.skip=true -f pom-%%a.xml
 )) 
-
 @pause
-
