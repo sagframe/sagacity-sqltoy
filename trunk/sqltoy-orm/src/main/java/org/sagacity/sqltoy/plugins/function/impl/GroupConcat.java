b7583b1190572eab953ef63c0883b389179bbd18
chenrenfei <zhongxuchen@gmail.com>
2019-11-05 22:02:05 +0800

    优化函数

---
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/plugins/function/impl/GroupConcat.java
@@ -0,0 +1,37 @@
+package org.sagacity.sqltoy.plugins.function.impl;
+
+import java.util.regex.Pattern;
+
+import org.sagacity.sqltoy.plugins.function.IFunction;
+import org.sagacity.sqltoy.utils.DataSourceUtils.DBType;
+
+/**
+ * 
+ * @author zhongxuchen
+ *
+ */
+public class GroupConcat extends IFunction {
+	private static Pattern regex = Pattern.compile("(?i)\\Wgroup_concat\\(");
+
+	@Override
+	public String dialects() {
+		return "";
+	}
+
+	@Override
+	public Pattern regex() {
+		return regex;
+	}
+
+	@Override
+	public String wrap(int dialect, String functionName, boolean hasArgs, String... args) {
+		if (dialect == DBType.POSTGRESQL || dialect == DBType.POSTGRESQL11 || dialect == DBType.POSTGRESQL10) {
+			if (args.length > 1)
+				return " array_to_string(ARRAY_AGG(" + args[0] + ")," + args[1] + ") ";
+			else
+				return " array_to_string(ARRAY_AGG(" + args[0] + "),',')";
+		}
+		return null;
+	}
+
+}
---
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/plugins/function/impl/If.java
@@ -0,0 +1,36 @@
+package org.sagacity.sqltoy.plugins.function.impl;
+
+import java.util.regex.Pattern;
+
+import org.sagacity.sqltoy.plugins.function.IFunction;
+import org.sagacity.sqltoy.utils.DataSourceUtils.DBType;
+
+/**
+ * 
+ * @author zhongxuchen
+ *
+ */
+public class If extends IFunction {
+	private static Pattern regex = Pattern.compile("(?i)\\Wif\\(");
+
+	@Override
+	public String dialects() {
+		return "";
+	}
+
+	@Override
+	public Pattern regex() {
+		return regex;
+	}
+
+	@Override
+	public String wrap(int dialect, String functionName, boolean hasArgs, String... args) {
+		if (dialect == DBType.MYSQL || dialect == DBType.MYSQL8) {
+			return null;
+		}
+		if (args.length < 3)
+			return null;
+		return " case when " + args[0] + " then " + args[1] + " else " + args[2] + " end";
+	}
+
+}
