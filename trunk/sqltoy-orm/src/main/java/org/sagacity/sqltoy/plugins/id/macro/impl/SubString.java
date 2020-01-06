2ed3483f5a48d42543dc17e8b1c4648436a921ab
chenrenfei <102309713@qq.com>
2020-01-06 17:35:44 +0800

    Update SubString.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/plugins/id/macro/impl/SubString.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/plugins/id/macro/impl/SubString.java
@@ -3,11 +3,14 @@ package org.sagacity.sqltoy.plugins.id.macro.impl;
 import java.util.HashMap;
 
 import org.sagacity.sqltoy.plugins.id.macro.AbstractMacro;
+import org.sagacity.sqltoy.utils.StringUtil;
 
 /**
+ * @project sagacity-sqltoy4.2
+ * @description 用于主键生成策略，根据依赖的字段值进行切割提取部分字符参与主键生成,用法:@substr(${colName},startIndex,length)
+ *              例如 @substr(${colName},0,2) 从第0位开始切去2位字符
+ * @author zhongxuchen
  * 
- * @author zhong
- *
  */
 public class SubString extends AbstractMacro {
 
@@ -16,7 +19,7 @@ public class SubString extends AbstractMacro {
 		if (params == null || params.length < 3)
 			return "";
 		Object value = keyValues.get(params[0].toLowerCase());
-		if (value == null)
+		if (StringUtil.isBlank(value))
 			return "";
 		String paramValue = value.toString();
 		int start = Integer.parseInt(params[1]);
@@ -31,19 +34,4 @@ public class SubString extends AbstractMacro {
 		return paramValue.substring(start);
 	}
 
-	/*public static void main(String[] args) {
-		String[] params = { "0H", "2", "2" };
-		String paramValue = params[0];
-		int start = Integer.parseInt(params[1]);
-		int length = Integer.parseInt(params[2]);
-		int realSize = (paramValue.length() - start) > length ? length : (paramValue.length() - start);
-		String result = "";
-		if (start >= paramValue.length())
-			result = "";
-		else if ((paramValue.length() - start) > length)
-			result = paramValue.substring(start, start + length);
-		else
-			result = paramValue.substring(start);
-		System.err.println(result);
-	}*/
 }
