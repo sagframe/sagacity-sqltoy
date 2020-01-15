769697bebf0f6bcac1630bcb9fdeb5ba21f2a844
chenrenfei <102309713@qq.com>
2020-01-15 10:51:51 +0800

    转移stringutil测试

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/utils/StringUtil.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/utils/StringUtil.java
@@ -783,23 +783,5 @@ public class StringUtil {
 		return beforeStr.concat(replaceBody).concat(endStr);
 	}
 
-	public static void main(String[] args) {
-		String tmp = CommonUtils.readFileAsString("classpath:/showcase.txt", "UTF-8");
-		String[] strs = StringUtil.splitExcludeSymMark(tmp, ",", SqlToyConstants.filters);
-		for (String s : strs) {
-			System.err.println("[" + s + "]");
-		}
-		// System.err.println(tmp);
-		// String regex = "(^\")|([^\\\\]\")";
-		// // String regex = "(^\\')|([^\\\\]\\')";
-		// Pattern p = Pattern.compile(regex);
-		// Matcher m = p.matcher(tmp);
-		// while (m.find()) {
-		// System.err.println(m.start());
-		// System.err.println(m.group());
-		// // System.err.println(m.group().substring(1));
-		// // System.err.println(tmp.charAt(m.start() - 1));
-		// }
-
-	}
+	
 }
