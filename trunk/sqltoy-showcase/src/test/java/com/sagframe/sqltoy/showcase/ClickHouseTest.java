6717a1ac229ddc074b796101a44b71c997d0bbce
chenrenfei <zhongxuchen@gmail.com>
2020-01-28 20:19:51 +0800

    增加showcase

---
+++ trunk/sqltoy-showcase/src/test/java/com/sagframe/sqltoy/showcase/ClickHouseTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.sagframe.sqltoy.showcase;
+
+import org.junit.Test;
+import org.sagacity.sqltoy.service.SqlToyCRUDService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @project sqltoy-boot-showcase
+ * @description 基于clickhouse的功能验证
+ * @author chenrenfei <a href="mailto:zhongxuchen@gmail.com">联系作者</a>
+ * @version id:ClickHouseTest.java,Revision:v1.0,Date:2020年1月22日
+ */
+public class ClickHouseTest {
+	@Autowired
+	private SqlToyCRUDService sqlToyCRUDService;
+
+	@Test
+	public void testInsert() {
+
+	}
+
+	@Test
+	public void testBatchInsert() {
+
+	}
+}
