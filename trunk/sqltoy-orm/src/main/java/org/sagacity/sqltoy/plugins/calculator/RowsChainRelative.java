1d1f789d19b4d749b0e4391ca7b77210b5956a8d
chenrenfei <102309713@qq.com>
2020-03-25 15:04:57 +0800

    Update RowsChainRelative.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/plugins/calculator/RowsChainRelative.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/plugins/calculator/RowsChainRelative.java
@@ -4,7 +4,12 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.sagacity.sqltoy.config.model.RowsChainRelativeModel;
-
+/**
+ * @project sqltoy-orm
+ * @description 对集合数据以行与行之间的比较(环比计算)
+ * @author renfei.chen <a href="mailto:zhongxuchen@hotmail.com">联系作者</a>
+ * @version id:RowsChainRelative.java,Revision:v1.0,Date:2020-3-25 上午10:08:15
+ */
 public class RowsChainRelative {
 	public static void process(RowsChainRelativeModel rowsRelativeModel, HashMap<String, Integer> labelIndexMap,
 			List result) {
