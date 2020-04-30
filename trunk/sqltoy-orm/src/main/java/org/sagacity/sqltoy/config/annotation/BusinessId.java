6b0a7ade63409ffab775421191b9ad912d0e3044
zhongxuchen <zhongxuchen@gmail.com>
2020-04-30 20:24:59 +0800

    增加保留字注解

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/config/annotation/BusinessId.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/config/annotation/BusinessId.java
@@ -9,14 +9,14 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Retention(RUNTIME)
-@Target(ElementType.FIELD)
 /**
  * @project sagacity-sqltoy4.0
  * @description 业务主键,可以充当主键(一般可能是多个字段联合充当)
  * @author chenrenfei <a href="mailto:zhongxuchen@gmail.com">联系作者</a>
  * @version id:BusinessId.java,Revision:v1.0,Date:2018年1月12日
  */
+@Retention(RUNTIME)
+@Target(ElementType.FIELD)
 public @interface BusinessId {
 	/**
 	 * 识别区分符号
---
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/config/annotation/ReservedWords.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.sagacity.sqltoy.config.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @project sagacity-sqltoy4.0
+ * @description 标注哪些字段是保留字,从而产生sql时增加对应的符号
+ * @author chenrenfei <a href="mailto:zhongxuchen@gmail.com">联系作者</a>
+ * @version id:ReservedWords.java,Revision:v1.0,Date:2020年4月30日
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ReservedWords {
+	/**
+	 * 哪些字段名称是保留字
+	 * 
+	 * @return
+	 */
+	String[] fields() default {};
+}
