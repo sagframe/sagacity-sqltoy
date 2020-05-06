3869bc2e654ab70674a3dca34ca393c3db207641
zhongxuchen <zhongxuchen@gmail.com>
2020-05-06 23:19:09 +0800

    Update StringUtil.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/utils/StringUtil.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/utils/StringUtil.java
@@ -582,11 +582,11 @@ public class StringUtil {
 				result[1] = source.indexOf(filter[1], result[0] + 1);
 			}
 		} else {
-			result[0] = StringUtil.matchIndex(source, pattern, skipIndex)[0];
+			result[0] = matchIndex(source, pattern, skipIndex)[0];
 			// 正则表达式有一个转义符号占一位
 			if (result[0] >= 0) {
 				result[0] = result[0] + 1;
-				result[1] = StringUtil.matchIndex(source, pattern, result[0] + 1)[0];
+				result[1] = matchIndex(source, pattern, result[0] + 1)[0];
 			}
 		}
 		while (result[1] > 0 && result[1] < splitIndex) {
@@ -596,11 +596,11 @@ public class StringUtil {
 					result[1] = source.indexOf(filter[1], result[0] + 1);
 				}
 			} else {
-				result[0] = StringUtil.matchIndex(source, pattern, result[1] + 1)[0];
+				result[0] = matchIndex(source, pattern, result[1] + 1)[0];
 				// 正则表达式有一个转义符号占一位
 				if (result[0] > 0) {
 					result[0] = result[0] + 1;
-					result[1] = StringUtil.matchIndex(source, pattern, result[0] + 1)[0];
+					result[1] = matchIndex(source, pattern, result[0] + 1)[0];
 				}
 			}
 		}
@@ -640,11 +640,11 @@ public class StringUtil {
 					endSignIndex = source.indexOf(endSign, beginSignIndex + 1);
 				}
 			} else {
-				beginSignIndex = StringUtil.matchIndex(source, pattern);
+				beginSignIndex = matchIndex(source, pattern);
 				// 转义符号占一位,开始位后移一位
 				if (beginSignIndex > -1) {
 					beginSignIndex = beginSignIndex + 1;
-					endSignIndex = StringUtil.matchIndex(source, pattern, beginSignIndex + 1)[0];
+					endSignIndex = matchIndex(source, pattern, beginSignIndex + 1)[0];
 					// 转义符号占一位,开始位后移一位
 					if (endSignIndex >= beginSignIndex + 1) {
 						endSignIndex = endSignIndex + 1;
