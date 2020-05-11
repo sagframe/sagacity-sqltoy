c53572b8ccae00e5719d385a6117071a78369fed
chenrenfei <102309713@qq.com>
2020-05-11 15:23:06 +0800

    Update EntityQuery.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/link/EntityQuery.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/link/EntityQuery.java
@@ -10,6 +10,8 @@ public class EntityQuery<T> implements Serializable {
 	private static final long serialVersionUID = 5223170071884950204L;
 	
 	private Class<T> resultType;
+	
+	//private List<>
 
 	public EntityQuery(Class<T> entityClass) {
 		this.resultType = entityClass;
