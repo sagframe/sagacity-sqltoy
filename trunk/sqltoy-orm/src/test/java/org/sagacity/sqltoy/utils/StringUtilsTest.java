d3c63f40abde8809ee5d98e36000ba917caabc4c
zhongxuchen <zhongxuchen@gmail.com>
2020-05-14 00:20:41 +0800

    Revert "优化"
    This reverts commit 11ad354df2e31726b44ee811d0001c597b4ff721.

--- trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/utils/StringUtilsTest.java
+++ trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/utils/StringUtilsTest.java
@@ -13,8 +13,16 @@ import org.sagacity.sqltoy.model.EntityQuery;
 public class StringUtilsTest {
 	public static void main(String[] args) {
 		// where中写条件，利用sqltoy自身特性#[] 实现值为null的判断，简化sql组装
+
 		// 第一种模式:直接传参
 		EntityQuery query = new EntityQuery();
+
+		// Type type = ((ParameterizedType)
+		// query.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+
+		// System.out.println(type);
+		// 在类的内部这样获取
+		// System.out.println(query.getTClass());
 		StringUtilsTest test = new StringUtilsTest();
 		test.selectList(query);
 //		// 第二种模式:对象传参
@@ -27,6 +35,11 @@ public class StringUtilsTest {
 
 	public void selectList(EntityQuery<StaffInfoVO> query) {
 		Class result = BeanUtil.getSuperClassGenricType(query.getClass(), 0);
+		// Type type = ((ParameterizedType)
+		// query.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+
 		System.out.println(result.getName());
+		// 在类的内部这样获取
+		// System.out.println(query.getTClass());
 	}
 }
