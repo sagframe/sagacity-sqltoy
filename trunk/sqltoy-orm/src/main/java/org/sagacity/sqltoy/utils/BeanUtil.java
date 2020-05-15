b8b157ab2af56cc34e7e2a77e1e05ebed347c701
zhongxuchen <zhongxuchen@gmail.com>
2020-05-15 23:30:35 +0800

    Update BeanUtil.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/utils/BeanUtil.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/utils/BeanUtil.java
@@ -1144,26 +1144,4 @@ public class BeanUtil {
 		}
 		return method.invoke(bean);
 	}
-
-	/**
-	 * @TODO 获取泛型的类型
-	 * @param clazz
-	 * @param index
-	 * @return
-	 * @throws IndexOutOfBoundsException
-	 */
-	public static Class getSuperClassGenricType(Class clazz, int index) throws IndexOutOfBoundsException {
-		Type genType = clazz.getGenericSuperclass();
-		if (!(genType instanceof ParameterizedType)) {
-			return Object.class;
-		}
-		Type[] params = ((ParameterizedType) genType).getActualTypeArguments();
-		if (index >= params.length || index < 0) {
-			return Object.class;
-		}
-		if (!(params[index] instanceof Class)) {
-			return Object.class;
-		}
-		return (Class) params[index];
-	}
 }
