b086291160a66f6c3712a5b649cbae3cef404813
zhongxuchen <zhongxuchen@gmail.com>
2020-05-17 14:18:14 +0800

    Update StringUtilsTest.java

--- trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/utils/StringUtilsTest.java
+++ trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/utils/StringUtilsTest.java
@@ -3,13 +3,21 @@
  */
 package org.sagacity.sqltoy.utils;
 
+import org.sagacity.sqltoy.SqlToyConstants;
+
 /**
  * @author zhongxuchen
  *
  */
 public class StringUtilsTest {
 	public static void main(String[] args) {
-
+		String source="#[testNum],'#,#0.00'";
+		
+		String[] result=StringUtil.splitExcludeSymMark(source, ",", SqlToyConstants.filters);
+		for(String str:result)
+		{
+			System.err.println("["+str+"]");
+		}
 	}
 
 }
