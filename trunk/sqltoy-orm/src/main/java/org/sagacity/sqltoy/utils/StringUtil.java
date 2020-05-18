dc7b8cdbbfafd431c02b85a237e79788c18ec9bc
zhongxuchen <zhongxuchen@gmail.com>
2020-05-18 15:18:57 +0800

    Update StringUtil.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/utils/StringUtil.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/utils/StringUtil.java
@@ -596,6 +596,7 @@ public class StringUtil {
 		} else if (filter[0].equals("\"")) {
 			pattern = twoQuotaPattern;
 		}
+		String tmp;
 		if (pattern == null) {
 			result[0] = source.indexOf(filter[0], skipIndex);
 			if (result[0] >= 0) {
@@ -603,17 +604,17 @@ public class StringUtil {
 			}
 		} else {
 			result[0] = matchIndex(source, pattern, skipIndex)[0];
-			// 正则表达式有一个转义符号占一位
 			if (result[0] >= 0) {
-				result[1] = getSymMarkIndex(filter[0], filter[1], source, result[0]);
-				if (result[0] > 0) {
+				tmp = source.substring(result[0], result[0] + 1);
+				if (!tmp.equals("'") && !tmp.equals("\"")) {
 					result[0] = result[0] + 1;
 				}
+				result[1] = getSymMarkIndex(filter[0], filter[1], source, result[0]);
 			}
 		}
 		while (result[1] > 0 && result[1] < splitIndex) {
 			if (pattern == null) {
-				//非正则表达式,往后移动一位
+				// 非正则表达式,往后移动一位
 				result[0] = source.indexOf(filter[0], result[1] + 1);
 				if (result[0] > 0) {
 					result[1] = getSymMarkIndex(filter[0], filter[1], source, result[0]);
@@ -621,12 +622,19 @@ public class StringUtil {
 					result[1] = -1;
 				}
 			} else {
-				//twoQuotaPattern 和 quotaPattern 表达式末尾匹配占用了2位长度,所以+2
-				result[0] = matchIndex(source, pattern, result[1] + 1)[0];
+				tmp = source.substring(result[1], result[1] + 1);
+				if (!tmp.equals("'") && !tmp.equals("\"")) {
+					result[0] = matchIndex(source, pattern, result[1] + 2)[0];
+				} else {
+					result[0] = matchIndex(source, pattern, result[1] + 1)[0];
+				}
 				// 正则表达式有一个转义符号占一位
 				if (result[0] > 0) {
+					tmp = source.substring(result[0], result[0] + 1);
+					if (!tmp.equals("'") && !tmp.equals("\"")) {
+						result[0] = result[0] + 1;
+					}
 					result[1] = getSymMarkIndex(filter[0], filter[1], source, result[0]);
-					result[0] = result[0] + 1;
 				} else {
 					result[1] = -1;
 				}
