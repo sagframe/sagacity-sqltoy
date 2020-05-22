4341dc1cb9c59d4c59b3ea5cc8e0e85400403c6c
chenrenfei <102309713@qq.com>
2020-05-22 14:13:20 +0800

    增强单元测试

--- trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/plugins/FunctionUtilTest.java
+++ trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/plugins/FunctionUtilTest.java
@@ -2,6 +2,7 @@ package org.sagacity.sqltoy.plugins;
 
 import java.util.Arrays;
 
+import org.junit.jupiter.api.Test;
 import org.sagacity.sqltoy.plugins.function.FunctionUtils;
 import org.sagacity.sqltoy.utils.CommonUtils;
 
@@ -9,7 +10,7 @@ public class FunctionUtilTest {
 	private final static String funPackage = "org.sagacity.sqltoy.plugins.function.impl.";
 
 	// 提供默认函数配置
-	public final static String[] functions = { /*
+	private final static String[] functions = { /*
 												 * funPackage.concat("SubStr"), funPackage.concat("Trim"),
 												 * funPackage.concat("Instr"), funPackage.concat("Concat"),
 												 * funPackage.concat("ConcatWs"), funPackage.concat("Nvl"),
@@ -17,16 +18,22 @@ public class FunctionUtilTest {
 												 * funPackage.concat("Length"), funPackage.concat("ToChar"),
 												 * funPackage.concat("If"),
 												 */
-			funPackage.concat("Instr")
+			funPackage.concat("Instr"), funPackage.concat("Concat")
 			// funPackage.concat("GroupConcat")
 	};
 
-	public static void main(String[] args) {
+	@Test
+	public void testInstr() {
 		FunctionUtils.setFunctionConverts(Arrays.asList(functions));
 		String sql = CommonUtils.readFileAsString("classpath:/scripts/instr_function.txt", "UTF-8");
-		// String sql = "select concat('\\'', t.`ORGAN_ID`, '\\'') from sys_organ_info t
-		// ";
+		String dialectSql = FunctionUtils.getDialectSql(sql, "oracle");
+		System.err.println(dialectSql);
+	}
 
+	@Test
+	public void testConcat() {
+		FunctionUtils.setFunctionConverts(Arrays.asList(functions));
+		String sql = "select concat('\\'', t.`ORGAN_ID`, '\\'') from sys_organ_info t";
 		String dialectSql = FunctionUtils.getDialectSql(sql, "oracle");
 		System.err.println(dialectSql);
 	}
--- trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/utils/StringUtilsTest.java
+++ trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/utils/StringUtilsTest.java
@@ -3,6 +3,9 @@
  */
 package org.sagacity.sqltoy.utils;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
 import org.sagacity.sqltoy.SqlToyConstants;
 
 /**
@@ -10,14 +13,35 @@ import org.sagacity.sqltoy.SqlToyConstants;
  *
  */
 public class StringUtilsTest {
-	public static void main(String[] args) {
-		String source="#[testNum],'#,#0.00'";
-		
-		String[] result=StringUtil.splitExcludeSymMark(source, ",", SqlToyConstants.filters);
-		for(String str:result)
-		{
-			System.err.println("["+str+"]");
+
+	@Test
+	public void testSplitExcludeSymMark1() {
+		String source = "#[testNum],'#,#0.00'";
+		String[] result = StringUtil.splitExcludeSymMark(source, ",", SqlToyConstants.filters);
+		assertArrayEquals(result, new String[] { "#[testNum]", "'#,#0.00'" });
+		source = ",'#,#0.00'";
+		result = StringUtil.splitExcludeSymMark(source, ",", SqlToyConstants.filters);
+		assertArrayEquals(result, new String[] { "", "'#,#0.00'" });
+
+		source = "'\\'', t.`ORGAN_ID`, '\\''";
+		result = StringUtil.splitExcludeSymMark(source, ",", SqlToyConstants.filters);
+		assertArrayEquals(result, new String[] { "'\\''", " t.`ORGAN_ID`", " '\\''" });
+
+		source = "orderNo,<td align=\"center\" rowspan=\"#[group('orderNo,').size()]\">,@dict(EC_PAY_TYPE,#[payType])</td>";
+		result = StringUtil.splitExcludeSymMark(source, ",", SqlToyConstants.filters);
+		assertArrayEquals(result,
+				new String[] { "orderNo", "<td align=\"center\" rowspan=\"#[group('orderNo,').size()]\">",
+						"@dict(EC_PAY_TYPE,#[payType])</td>" });
+		source = "reportId=\"RPT_DEMO_005\",chart-index=\"1\",style=\"width:49%;height:350px;display:inline-block;\"";
+		result = StringUtil.splitExcludeSymMark(source, ",", SqlToyConstants.filters);
+		assertArrayEquals(result, new String[] { "reportId=\"RPT_DEMO_005\"", "chart-index=\"1\"",
+				"style=\"width:49%;height:350px;display:inline-block;\"" });
+		source = "a,\"\"\",\",a";
+		result = StringUtil.splitExcludeSymMark(source, ",", SqlToyConstants.filters);
+		for (String s : result) {
+			System.err.println("[" + s.trim() + "]");
 		}
+		assertArrayEquals(result, new String[] { "a", "\"\"\",\"", "a" });
 	}
 
 }
