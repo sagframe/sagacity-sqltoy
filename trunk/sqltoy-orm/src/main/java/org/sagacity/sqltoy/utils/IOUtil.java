0970f6cb55e2d826e77a94103ee313581dbcd011
zhongxuchen <zhongxuchen@gmail.com>
2020-05-30 11:15:35 +0800

    Update IOUtil.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/utils/IOUtil.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/utils/IOUtil.java
@@ -3,23 +3,17 @@
  */
 package org.sagacity.sqltoy.utils;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @project sagacity-core
@@ -29,20 +23,16 @@ import org.slf4j.LoggerFactory;
  */
 @SuppressWarnings("rawtypes")
 public class IOUtil {
-	/**
-	 * 定义日志
-	 */
-	private final static Logger logger = LoggerFactory.getLogger(IOUtil.class);
 
 	/**
 	 * 转换String为InputStream
-	 *
+	 * 
 	 * @param str
 	 * @param charset
 	 * @return
 	 * @throws Exception
 	 */
-	public static InputStream string2InputStream(String str, String charset) throws Exception {
+	public static InputStream strToInputStream(String str, String charset) throws Exception {
 		if (charset != null) {
 			return new ByteArrayInputStream(str.getBytes(charset));
 		}
@@ -50,50 +40,6 @@ public class IOUtil {
 	}
 
 	/**
-	 * 转换InputStream为String
-	 *
-	 * @param is
-	 * @param encoding
-	 * @return
-	 */
-	public static String inputStream2String(InputStream is, String encoding) {
-		if (null == is) {
-			return null;
-		}
-		StringBuilder buffer = new StringBuilder();
-		BufferedReader in = null;
-		try {
-			if (StringUtil.isNotBlank(encoding)) {
-				in = new BufferedReader(new InputStreamReader(is, encoding));
-			} else {
-				in = new BufferedReader(new InputStreamReader(is));
-			}
-			String line = "";
-			while ((line = in.readLine()) != null) {
-				buffer.append(line);
-				buffer.append("\r\n");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			logger.error(e.getMessage());
-		} finally {
-			closeQuietly(in);
-		}
-		return buffer.toString();
-	}
-
-	/**
-	 * outputStream 转换为InputStream
-	 *
-	 * @param out
-	 * @return
-	 */
-	public static InputStream convert2InputStream(ByteArrayOutputStream out) {
-		return new ByteArrayInputStream(out.toByteArray());
-	}
-
-	/**
-	 *
 	 * @todo 将对象转换成字节数组
 	 * @param obj
 	 * @return
@@ -141,7 +87,7 @@ public class IOUtil {
 	}
 
 	/**
-	 * @todo 字节数组转换成对象
+	 * @todo 字节数组转换成对象,一般用于对象序列化
 	 * @param inStream
 	 * @return
 	 */
@@ -224,80 +170,6 @@ public class IOUtil {
 		} finally {
 			closeQuietly(out, bOut);
 		}
-	}
-
-	public static Object readObject(SocketChannel sc) throws IOException {
-		int INT_LENGTH = 4;
-		Object readObj = null;
-		int bytes = -1;
-		int bytesTotal = 0;
-		ByteBuffer bufLen = null;
-		ByteBuffer bufTemp = null;
-		ByteBuffer bufMsg = null;
-
-		/* 读取Object的长度 */
-		bufLen = ByteBuffer.allocateDirect(INT_LENGTH);
-		bufTemp = ByteBuffer.allocateDirect(INT_LENGTH);
-		/* 读取INT_LENGTH个字节作为Object的长度 */
-		while (0 < (bytes = sc.read(bufTemp))) {
-			bytesTotal += bytes;
-			bufTemp.flip();
-			bufLen.put(bufTemp);
-			if (bytesTotal < INT_LENGTH) {
-				// 长度读取未完
-				bufTemp = ByteBuffer.allocateDirect(INT_LENGTH - bytesTotal);
-			} else {
-				break;
-			}
-		}
-		if (0 == bytesTotal) {
-			return null;
-		}
-		if (bytesTotal < INT_LENGTH) {
-			// 读取到的字节数少于约定的字节数，作为通信ERROR处理
-			throw new IOException("Object长度读取出错。目前读取字节数：" + bytesTotal);
-		}
-
-		int len = 0;
-		try {
-			bufLen.flip();
-			len = bufLen.getInt();
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new IOException("读取到的字节非Object长度");
-		}
-		if (0 == len) {
-			return null;
-		}
-
-		/* 读取Object自身 */
-		// System.err.println(len);
-		bufTemp = ByteBuffer.allocate(len);
-		bufMsg = ByteBuffer.allocate(len);
-		bytes = -1;
-		bytesTotal = 0;
-		/* 读取len（前面取到的长度）个字节作为Object自身 */
-		while (0 < (bytes = sc.read(bufTemp))) {
-			bytesTotal += bytes;
-			bufTemp.flip();
-			bufMsg.put(bufTemp);
-			if (bytesTotal < len) {
-				// Object自身读取未完了
-				bufTemp = ByteBuffer.allocateDirect(len - bytesTotal);
-			} else {
-				break;
-			}
-		}
-		if (bytesTotal < len) {
-			// 读取的字节数少于约定的字节数，作为通信ERROR处理
-			throw new IOException("Object读取出错");
-		}
-
-		bufMsg.flip();
-		byte[] buf = new byte[bufMsg.limit()];
-		bufMsg.get(buf);
-		readObj = bytesToObject(buf);
-		return readObj;
 	}
 
 	/**
