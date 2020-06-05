ee81745dfe6f88fd0c0c7dab4e7e66e8bcf6e3d4
chenrenfei <102309713@qq.com>
2020-06-05 18:38:57 +0800

    Update DMDialect.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/dialect/impl/DMDialect.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/dialect/impl/DMDialect.java
@@ -32,7 +32,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * @project sqltoy-orm
- * @description 国产达梦数据库方言支持(从DM8开始支持)
+ * @description 国产达梦数据库方言支持(从DM8开始支持),dm数据库跟oracle差异:在merge into里面使用nvl报错
  * @author renfei.chen <a href="mailto:zhongxuchen@hotmail.com">联系作者</a>
  * @version Revision:v1.0,Date:2020-06-05
  * @Modification Date:2020-06-05 初始创建达梦数据库支持
