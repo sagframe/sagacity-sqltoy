645b7bb394e26db1b952478862fde380a9fe5a32
chenrenfei <102309713@qq.com>
2020-07-15 16:42:17 +0800

    新增quickstart

---
+++ trunk/sqltoy-quickstart/quickvo/quickvo.bat
@@ -0,0 +1,4 @@
+﻿@echo ============== start generate vo and abstractvo file ==================
+java -cp ./libs/* org.sagacity.quickvo.QuickVOStart quickvo.xml
+@echo ============== end generate vo and abstractvo file ====================
+cmd
