33b6c03f64f76e0fabc6d3b6b2c3bc9cbb5978af
chenrenfei <102309713@qq.com>
2020-07-17 15:53:09 +0800

    Update StaffInfoServiceTest.java

--- trunk/sqltoy-quickstart/src/test/java/com/sqltoy/quickstart/StaffInfoServiceTest.java
+++ trunk/sqltoy-quickstart/src/test/java/com/sqltoy/quickstart/StaffInfoServiceTest.java
@@ -3,11 +3,11 @@ package com.sqltoy.quickstart;
 /**
  * 
  * @project sqltoy-quickstart
- * @description 请在此说明类的功能
+ * @description 通过员工信息表来演示常规的crud
  * @author zhong
  * @version v1.0, Date:2020年7月17日
  * @modify 2020年7月17日,修改说明
  */
-public class ServiceCrudTest {
+public class StaffInfoServiceTest {
 
 }
