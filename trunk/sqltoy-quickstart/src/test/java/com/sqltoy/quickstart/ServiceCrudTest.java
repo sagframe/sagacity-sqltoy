a026153c38e33daeafa0c1c681168f420feb9843
chenrenfei <102309713@qq.com>
2020-07-17 15:52:25 +0800

    提交

--- trunk/sqltoy-quickstart/src/test/java/com/sqltoy/quickstart/ServiceCrudTest.java
+++ trunk/sqltoy-quickstart/src/test/java/com/sqltoy/quickstart/ServiceCrudTest.java
@@ -1,12 +1,13 @@
-package com.sqltoy.quickstart.service;
+package com.sqltoy.quickstart;
 
 /**
+ * 
  * @project sqltoy-quickstart
  * @description 请在此说明类的功能
- * @author zhong 
+ * @author zhong
  * @version v1.0, Date:2020年7月17日
  * @modify 2020年7月17日,修改说明
  */
-public interface StaffInfoService {
+public class ServiceCrudTest {
 
 }
