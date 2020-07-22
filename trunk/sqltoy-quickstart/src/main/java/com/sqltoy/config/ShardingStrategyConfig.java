1818c0fd01100be229206241d8f4a7b48e152276
zhongxuchen <zhongxuchen@gmail.com>
2020-07-22 10:34:40 +0800

    Update ShardingStrategyConfig.java

--- trunk/sqltoy-quickstart/src/main/java/com/sqltoy/config/ShardingStrategyConfig.java
+++ trunk/sqltoy-quickstart/src/main/java/com/sqltoy/config/ShardingStrategyConfig.java
@@ -7,30 +7,57 @@ import java.util.HashMap;
 
 import org.sagacity.sqltoy.plugins.sharding.ShardingStrategy;
 import org.sagacity.sqltoy.plugins.sharding.impl.DefaultShardingStrategy;
+import org.sagacity.sqltoy.plugins.sharding.impl.HashShardingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @project sqltoy-quickstart
- * @description 请在此说明类的功能
+ * @description 演示一个分表的策略(分库的类似)
  * @author zhongxuchen
  * @version v1.0, Date:2020-7-22
  * @modify 2020-7-22,修改说明
  */
 @Configuration
-public class ShardingTableStrategy {
+public class ShardingStrategyConfig {
 
-	@Bean("historyTableStrategy")
-	public ShardingStrategy historyTableStrategy() {
+	/**
+	 * @TODO 演示实时表和历史表分表效果
+	 * @return
+	 */
+	@Bean("hisTableStrategy")
+	public ShardingStrategy hisTableStrategy() {
 		DefaultShardingStrategy strategy = new DefaultShardingStrategy();
 		// 分14天表和历史表,也可以1,15,90 表示多个间隔
 		strategy.setDays("14");
 		strategy.setDateParams("createTime,beginDate,bizDate,beginTime,bizTime");
 		HashMap<String, String> tableMap = new HashMap<String, String>();
 		// 写sql时可以直接用15d的表
-		//value可用逗号分隔,跟days对应 ,SQLTOY_TRANS_INFO_90,SQLTOY_TRANS_INFO_HIS
+		// value可用逗号分隔,跟days对应 ,SQLTOY_TRANS_INFO_90,SQLTOY_TRANS_INFO_HIS
 		tableMap.put("SQLTOY_TRANS_INFO_15D", "SQLTOY_TRANS_INFO_HIS");
 		strategy.setTableNamesMap(tableMap);
+		return strategy;
+	}
+
+	/**
+	 * @TODO 按权重进行分库
+	 * @return
+	 */
+	@Bean("weightDataSource")
+	public ShardingStrategy weightDataSource() {
+		DefaultShardingStrategy strategy = new DefaultShardingStrategy();
+
+		return strategy;
+	}
+
+	/**
+	 * @TODO 通过hash取模方式分库
+	 * @return
+	 */
+	@Bean("hashDataSource")
+	public ShardingStrategy hashDataSource() {
+		HashShardingStrategy strategy = new HashShardingStrategy();
+
 		return strategy;
 	}
 }
