ae8fdff4d92a3717aa48bd0812ae0639a06a9699
zhongxuchen <zhongxuchen@gmail.com>
2020-07-22 17:20:45 +0800

    增加机构维护演示树结构表维护

---
+++ trunk/sqltoy-quickstart/src/main/java/com/sqltoy/quickstart/service/impl/OrganInfoServiceImpl.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.sqltoy.quickstart.service.impl;
+
+import com.sqltoy.quickstart.service.OrganInfoService;
+import com.sqltoy.quickstart.vo.OrganInfoVO;
+
+/**
+ * @project sqltoy-quickstart
+ * @description 请在此说明类的功能
+ * @author zhongxuchen 
+ * @version v1.0, Date:2020-7-22
+ * @modify 2020-7-22,修改说明
+*/
+public class OrganInfoServiceImpl implements OrganInfoService {
+
+	@Override
+	public Long saveOrganInfo(OrganInfoVO organInfoVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
