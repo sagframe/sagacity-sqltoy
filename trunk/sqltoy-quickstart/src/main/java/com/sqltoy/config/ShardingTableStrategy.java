5613fdbeb139bf74d1cab8d540548a12df573fdc
zhongxuchen <zhongxuchen@gmail.com>
2020-07-22 10:28:24 +0800

    Update ShardingTableStrategy.java

--- trunk/sqltoy-quickstart/src/main/java/com/sqltoy/config/ShardingTableStrategy.java
+++ trunk/sqltoy-quickstart/src/main/java/com/sqltoy/config/ShardingTableStrategy.java
@@ -23,11 +23,12 @@ public class ShardingTableStrategy {
 	@Bean("historyTableStrategy")
 	public ShardingStrategy historyTableStrategy() {
 		DefaultShardingStrategy strategy = new DefaultShardingStrategy();
-		//
+		// 分14天表和历史表,也可以1,15,90 表示多个间隔
 		strategy.setDays("14");
 		strategy.setDateParams("createTime,beginDate,bizDate,beginTime,bizTime");
 		HashMap<String, String> tableMap = new HashMap<String, String>();
 		// 写sql时可以直接用15d的表
+		//value可用逗号分隔,跟days对应 ,SQLTOY_TRANS_INFO_90,SQLTOY_TRANS_INFO_HIS
 		tableMap.put("SQLTOY_TRANS_INFO_15D", "SQLTOY_TRANS_INFO_HIS");
 		strategy.setTableNamesMap(tableMap);
 		return strategy;
