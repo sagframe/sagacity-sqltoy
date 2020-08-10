4fa43b7d5fd2673646c06f76fcd904056a82e13d
chenrenfei <102309713@qq.com>
2020-08-10 13:58:31 +0800

    Update DTOEntityMapModel.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/model/DTOEntityMapModel.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/model/DTOEntityMapModel.java
@@ -5,12 +5,13 @@ package org.sagacity.sqltoy.model;
 
 import java.io.Serializable;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
- * @author zhong
- *
+ * @project sagacity-sqltoy
+ * @description DTO(或VO)和POJO 属性映射关系模型,便于放入缓存快速提取
+ * @author zhongxuchen@gmail.com
+ * @version v1.0, Date:2020年8月8日
+ * @modify 2020年8月8日,修改说明
  */
 public class DTOEntityMapModel implements Serializable {
 
@@ -19,21 +20,19 @@ public class DTOEntityMapModel implements Serializable {
 	 */
 	private static final long serialVersionUID = 8851171361533403505L;
 
+	//dto
 	public String dtoClassName;
-
-	public String pojoClassName;
 
 	public String[] dtoProps;
 
-	public Map<String, Integer> dtoPropsIndex = new HashMap<String, Integer>();
 
 	public Method[] dtoSetMethods;
 
 	public Method[] dtoGetMethods;
 
+	//pojo entity
+	public String pojoClassName;
 	public String[] pojoProps;
-
-	public Map<String, Integer> pojoPropsIndex = new HashMap<String, Integer>();
 
 	public Method[] pojoSetMethods;
 
