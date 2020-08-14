7a491d44fb2f066c1358cebc0b8494462572db8b
zhongxuchen <zhongxuchen@gmail.com>
2020-08-15 07:08:07 +0800

    完善单元测试

--- trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/utils/PageOptimizeThread.java
+++ trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/utils/PageOptimizeThread.java
@@ -4,29 +4,33 @@ import org.sagacity.sqltoy.config.model.PageOptimize;
 import org.sagacity.sqltoy.config.model.SqlToyConfig;
 import org.sagacity.sqltoy.dialect.utils.PageOptimizeUtils;
 
+/**
+ * 模拟单个用户不停的组合查询
+ * 
+ * @author zhong
+ *
+ */
 public class PageOptimizeThread extends Thread {
 
-	private int index = 0;
+	private SqlToyConfig sqlToyConfig;
+	private PageOptimize pageOptimize;
+	private int userId;
 
-	public PageOptimizeThread(int i) {
-		this.index = i;
+	public PageOptimizeThread(SqlToyConfig sqlToyConfig, PageOptimize pageOptimize, int userId) {
+		this.sqlToyConfig = sqlToyConfig;
+		this.pageOptimize = pageOptimize;
+		this.userId = userId;
 	}
 
 	@Override
 	public void run() {
-		SqlToyConfig sqlToyConfig = new SqlToyConfig("mysql");
-		sqlToyConfig.setId("sqltoy_showcase");
-		PageOptimize pageOptimize = new PageOptimize();
-		//60秒
-		pageOptimize.aliveSeconds(60);
-		//100个
-		pageOptimize.aliveMax(200);
+
 		while (true) {
-			//验证超量则需要随机记录>aliveMax,验证超时尽量将量控制在aliveMax边缘
+			// 验证超量则需要随机记录>aliveMax,验证超时尽量将量控制在aliveMax边缘
 			String key = "key_" + NumberUtil.getRandomNum(1, 250);
 			try {
-				//每次操作间隔在3~50秒之间
-				Thread.sleep(NumberUtil.getRandomNum(3, 25)*1000);
+				// 每次操作间隔在3~50秒之间
+				Thread.sleep(NumberUtil.getRandomNum(3, 25) * 1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
