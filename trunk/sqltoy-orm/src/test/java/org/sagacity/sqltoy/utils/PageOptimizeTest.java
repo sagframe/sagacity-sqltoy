ed1f09025a574b58ac96f0023ca91fe4cbc77c83
zhongxuchen <zhongxuchen@gmail.com>
2020-08-14 22:12:58 +0800

    Update PageOptimizeTest.java

--- trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/utils/PageOptimizeTest.java
+++ trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/utils/PageOptimizeTest.java
@@ -1,5 +1,10 @@
 package org.sagacity.sqltoy.utils;
 
+/**
+ * @TODO 针对分页优化进行多线程模拟测试
+ * @author zhongxuchen
+ *
+ */
 public class PageOptimizeTest {
 	public static void main(String[] args) {
 		//模仿60个用户
