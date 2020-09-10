9ec06560db77d291181262ad5511e099aef334fe
zhongxuchen <zhongxuchen@gmail.com>
2020-09-10 23:52:56 +0800

    Update NamedValuesModel.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/model/NamedValuesModel.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/model/NamedValuesModel.java
@@ -3,9 +3,11 @@ package org.sagacity.sqltoy.model;
 import java.io.Serializable;
 
 /**
- * 
- * @author zhongxuchen
- *
+ * @project sagacity-sqltoy
+ * @description 用于存放map转换后的named和values数据模型
+ * @author zhongxuchen@hotmail.com
+ * @version v1.0, Date:2020-9-10
+ * @modify 2020-9-10,修改说明
  */
 public class NamedValuesModel implements Serializable {
 
@@ -14,8 +16,14 @@ public class NamedValuesModel implements Serializable {
 	 */
 	private static final long serialVersionUID = 4776060558293466261L;
 	
+	/**
+	 * 参数名称
+	 */
 	private String[] paramNames;
 	
+	/**
+	 * 参数值
+	 */
 	private Object[] paramValues;
 
 	public String[] getParamNames() {
