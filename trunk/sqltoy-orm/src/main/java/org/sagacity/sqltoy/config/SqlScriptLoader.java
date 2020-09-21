304adbfb9470a452abb1cbb609a9452251b3fd65
zhongxuchen <zhongxuchen@gmail.com>
2020-09-21 22:05:48 +0800

    Update SqlScriptLoader.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/config/SqlScriptLoader.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/config/SqlScriptLoader.java
@@ -204,14 +204,12 @@ public class SqlScriptLoader {
 				// 兼容一下sqlserver的命名
 				if (result == null && realDialect.equals(Dialect.SQLSERVER)) {
 					result = sqlCache.get((sqlKey.concat("_mssql")));
-					// dialect_sqlId
 					if (result == null) {
 						result = sqlCache.get("mssql_".concat(sqlKey));
 					}
 				} // 兼容一下postgres的命名
 				if (result == null && realDialect.equals(Dialect.POSTGRESQL)) {
 					result = sqlCache.get((sqlKey.concat("_postgres")));
-					// dialect_sqlId
 					if (result == null) {
 						result = sqlCache.get("postgres_".concat(sqlKey));
 					}
