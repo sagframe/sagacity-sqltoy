5e47b778cb736a46e9f2fee061b924a057f518f4
zhongxuchen <zhongxuchen@gmail.com>
2020-09-22 22:22:29 +0800

    Update SqlConfigParseUtils.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/config/SqlConfigParseUtils.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/config/SqlConfigParseUtils.java
@@ -519,6 +519,7 @@ public class SqlConfigParseUtils {
 		}
 	}
 	
+	//update 2020-09-22 增加sql中的循环功能,避免极为特殊场景下不必要的争议
 	//格式:loop(:loopArgs,loopContent) 和 loop(:loopArgs,loopContent,linkSign) 两种
 	// #[or @loop(:beginDates,'(startTime between :beginDates[i] and endDates[i])',or)]
 	private static String processLoop(String queryStr, String[] paramsNamed, Object[] paramsValue) {
