0953d681f1cee3fe420f1fa28a475deb797b956d
chenrenfei <102309713@qq.com>
2020-09-24 13:23:04 +0800

    Update SqlLoop.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/plugins/id/macro/impl/SqlLoop.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/plugins/id/macro/impl/SqlLoop.java
@@ -18,7 +18,7 @@ import org.sagacity.sqltoy.utils.DateUtil;
 
 /**
  * @project sagacity-sqltoy
- * @description 此类不用于主键策略的配置,提供在sql中通过@loop(:args,loopContent,linkSign)
+ * @description 此类不用于主键策略的配置,提供在sql中通过@loop(:args,loopContent,linkSign,start,end)
  *              函数来循环组织sql(借用主键里面的宏工具来完成@loop处理)
  * @author zhongxuchen
  * @version v1.0, Date:2020-9-23
@@ -41,21 +41,36 @@ public class SqlLoop extends AbstractMacro {
 			}
 			params[i] = varStr;
 		}
-		//循环依据的数组参数
+		// 循环依据的数组参数
 		String loopParam = params[0].trim();
 		// 剔除:符号
 		if (loopParam.startsWith(":")) {
 			loopParam = loopParam.substring(1).trim();
 		}
-		//循环内容
+		// 循环内容
 		String loopContent = params[1];
-		//循环连接符号(字符串)
+		// 循环连接符号(字符串)
 		String linkSign = (params.length > 2) ? params[2] : " ";
-		
-		//获取循环依据的参数数组值
+
+		// 获取循环依据的参数数组值
 		Object[] loopValues = CollectionUtil.convertArray(keyValues.get(loopParam));
+
 		if (loopValues == null || loopValues.length == 0) {
 			return " ";
+		}
+		int start = 0;
+		int end = loopValues.length;
+		if (params.length > 3) {
+			start = Integer.parseInt(params[3].trim());
+		}
+		if (start > loopValues.length - 1) {
+			return " ";
+		}
+		if (params.length > 4) {
+			end = Integer.parseInt(params[4].trim());
+		}
+		if (end >= loopValues.length) {
+			end = loopValues.length;
 		}
 		// 提取循环体内的参数对应的值
 		List<String> keys = new ArrayList<String>();
@@ -68,7 +83,7 @@ public class SqlLoop extends AbstractMacro {
 			// 统一标准为paramName[i]模式
 			if (lowContent.contains(":" + key + "[i]") || lowContent.contains(":" + key + "[index]")) {
 				keys.add(key);
-				//统一转为key.lowCase[i]模式
+				// 统一转为key.lowCase[i]模式
 				loopContent = loopContent.replaceAll("(?i)\\:" + key + "\\[index\\]", ":" + key + "[i]");
 				loopContent = loopContent.replaceAll("(?i)\\:" + key + "\\[i\\]", ":" + key + "[i]");
 				regParamValues.add(CollectionUtil.convertArray(keyValues.get(key)));
@@ -79,7 +94,7 @@ public class SqlLoop extends AbstractMacro {
 		String loopStr;
 		Object paramValue;
 		String valueStr;
-		for (int i = 0; i < loopValues.length; i++) {
+		for (int i = start; i < end; i++) {
 			loopStr = loopContent;
 			if (i > 0) {
 				result.append(" ");
