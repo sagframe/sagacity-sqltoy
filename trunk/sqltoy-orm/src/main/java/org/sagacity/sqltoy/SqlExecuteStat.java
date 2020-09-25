83aa6cc3323a0c8a7dc8fc161176153a8473e8b3
chenrenfei <102309713@qq.com>
2020-09-25 14:12:42 +0800

    Update SqlExecuteStat.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/SqlExecuteStat.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/SqlExecuteStat.java
@@ -119,7 +119,7 @@ public class SqlExecuteStat {
 			}
 			long overTime = sqlTrace.getExecuteTime() - printSqlTimeoutMillis;
 			// sql执行超过阀值记录日志为软件优化提供依据
-			if (overTime >= 0 && sqlTrace.getStart() != null) {
+			if (overTime >= 0) {
 				sqlTrace.setOverTime(true);
 				sqlTrace.addLog("slowSql执行超时", "耗时(毫秒):{} >={} (阀值)!", sqlTrace.getExecuteTime(),
 						printSqlTimeoutMillis);
