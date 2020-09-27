1944727a658785a55f6dbf9b33c2e5ce1cc027c9
chenrenfei <102309713@qq.com>
2020-09-27 10:18:53 +0800

    Update TreeIdAndPidGet.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/callback/TreeIdAndPidGet.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/callback/TreeIdAndPidGet.java
@@ -3,9 +3,8 @@ package org.sagacity.sqltoy.callback;
 /**
  * @project sagacity-core
  * @description 树形结构数据处理的反调接口
- * @author chenrenfei $<a href="mailto:zhongxuchen@hotmail.com">联系作者</a>$
- * @version $id:TreeSetCallbackHandler.java,Revision:v1.0,Date:2010-9-13
- *          下午10:08:23 $
+ * @author chenrenfei <a href="mailto:zhongxuchen@hotmail.com">联系作者</a>
+ * @version id:TreeIdAndPidGet.java,Revision:v1.0,Date:2010-9-13
  */
 @FunctionalInterface
 public interface TreeIdAndPidGet<T> {
