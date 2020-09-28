82f851f72f56dc1b59c0aec1078a10b12d422774
chenrenfei <102309713@qq.com>
2020-09-28 16:45:39 +0800

    Update SqlToyCRUDServiceImpl.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/service/impl/SqlToyCRUDServiceImpl.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/service/impl/SqlToyCRUDServiceImpl.java
@@ -25,7 +25,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
