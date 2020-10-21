1925c8438c61483d3a3e44b38ce4b68e24687085
chenrenfei <102309713@qq.com>
2020-10-21 12:40:29 +0800

    更新

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/callback/InsertRowCallbackHandler.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/callback/InsertRowCallbackHandler.java
@@ -8,10 +8,11 @@ import java.sql.SQLException;
 
 /**
  * @project sagacity-sqltoy
- * @description 批量行数据插入反调抽象类定义
+ * @description 批量行数据插入反调抽象类定义(实际极少使用,留给在超极端场景下自定义pst处理)
  * @author chenrf <a href="mailto:zhongxuchen@hotmail.com">联系作者</a>
  * @version id:InsertRowCallbackHandler.java,Revision:v1.0,Date:2010-1-5
  */
+@Deprecated
 @FunctionalInterface
 public interface InsertRowCallbackHandler {
 	/**
