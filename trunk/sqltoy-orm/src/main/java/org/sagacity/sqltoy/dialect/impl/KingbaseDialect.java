73dbf51e23ebbf5956aa955ac56b3f2239a712c9
chenrenfei <102309713@qq.com>
2020-11-06 13:44:11 +0800

    更新准备支持kingbase数据库

---
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/dialect/impl/KingbaseDialect.java
@@ -0,0 +1,190 @@
+/**
+ * 
+ */
+package org.sagacity.sqltoy.dialect.impl;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.List;
+
+import org.sagacity.sqltoy.SqlToyContext;
+import org.sagacity.sqltoy.callback.ReflectPropertyHandler;
+import org.sagacity.sqltoy.callback.RowCallbackHandler;
+import org.sagacity.sqltoy.callback.UpdateRowHandler;
+import org.sagacity.sqltoy.config.model.SqlToyConfig;
+import org.sagacity.sqltoy.dialect.Dialect;
+import org.sagacity.sqltoy.executor.QueryExecutor;
+import org.sagacity.sqltoy.model.LockMode;
+import org.sagacity.sqltoy.model.QueryResult;
+import org.sagacity.sqltoy.model.StoreResult;
+
+/**
+ * @project sagacity-sqltoy
+ * @description 北大金仓数据库方言支持
+ * @author zhongxuchen
+ * @version v1.0, Date:2020-11-6
+ * @modify 2020-11-6,修改说明
+ */
+public class KingbaseDialect implements Dialect {
+
+	@Override
+	public boolean isUnique(SqlToyContext sqlToyContext, Serializable entity, String[] paramsNamed, Connection conn,
+			Integer dbType, String tableName) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public QueryResult getRandomResult(SqlToyContext sqlToyContext, SqlToyConfig sqlToyConfig,
+			QueryExecutor queryExecutor, Long totalCount, Long randomCount, Connection conn, Integer dbType,
+			String dialect) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public QueryResult findPageBySql(SqlToyContext sqlToyContext, SqlToyConfig sqlToyConfig,
+			QueryExecutor queryExecutor, Long pageNo, Integer pageSize, Connection conn, Integer dbType, String dialect)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public QueryResult findTopBySql(SqlToyContext sqlToyContext, SqlToyConfig sqlToyConfig, QueryExecutor queryExecutor,
+			Integer topSize, Connection conn, Integer dbType, String dialect) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public QueryResult findBySql(SqlToyContext sqlToyContext, SqlToyConfig sqlToyConfig, String sql,
+			Object[] paramsValue, RowCallbackHandler rowCallbackHandler, Connection conn, LockMode lockMode,
+			Integer dbType, String dialect, int fetchSize, int maxRows) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long getCountBySql(SqlToyContext sqlToyContext, SqlToyConfig sqlToyConfig, String sql, Object[] paramsValue,
+			boolean isLastSql, Connection conn, Integer dbType, String dialect) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Serializable load(SqlToyContext sqlToyContext, Serializable entity, List<Class> cascadeTypes,
+			LockMode lockMode, Connection conn, Integer dbType, String dialect, String tableName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<?> loadAll(SqlToyContext sqlToyContext, List<?> entities, List<Class> cascadeTypes, LockMode lockMode,
+			Connection conn, Integer dbType, String dialect, String tableName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object save(SqlToyContext sqlToyContext, Serializable entity, Connection conn, Integer dbType,
+			String dialect, String tableName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long saveAll(SqlToyContext sqlToyContext, List<?> entities, int batchSize,
+			ReflectPropertyHandler reflectPropertyHandler, Connection conn, Integer dbType, String dialect,
+			Boolean autoCommit, String tableName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long update(SqlToyContext sqlToyContext, Serializable entity, String[] forceUpdateFields, boolean cascade,
+			Class[] forceCascadeClass, HashMap<Class, String[]> subTableForceUpdateProps, Connection conn,
+			Integer dbType, String dialect, String tableName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long updateAll(SqlToyContext sqlToyContext, List<?> entities, int batchSize, String[] forceUpdateFields,
+			ReflectPropertyHandler reflectPropertyHandler, Connection conn, Integer dbType, String dialect,
+			Boolean autoCommit, String tableName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long saveOrUpdate(SqlToyContext sqlToyContext, Serializable entity, String[] forceUpdateFields,
+			Connection conn, Integer dbType, String dialect, Boolean autoCommit, String tableName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long saveOrUpdateAll(SqlToyContext sqlToyContext, List<?> entities, int batchSize,
+			ReflectPropertyHandler reflectPropertyHandler, String[] forceUpdateFields, Connection conn, Integer dbType,
+			String dialect, Boolean autoCommit, String tableName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long saveAllIgnoreExist(SqlToyContext sqlToyContext, List<?> entities, int batchSize,
+			ReflectPropertyHandler reflectPropertyHandler, Connection conn, Integer dbType, String dialect,
+			Boolean autoCommit, String tableName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long delete(SqlToyContext sqlToyContext, Serializable entity, Connection conn, Integer dbType,
+			String dialect, String tableName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long deleteAll(SqlToyContext sqlToyContext, List<?> entities, int batchSize, Connection conn, Integer dbType,
+			String dialect, Boolean autoCommit, String tableName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public QueryResult updateFetch(SqlToyContext sqlToyContext, SqlToyConfig sqlToyConfig, String sql,
+			Object[] paramValues, UpdateRowHandler updateRowHandler, Connection conn, Integer dbType, String dialect)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public QueryResult updateFetchTop(SqlToyContext sqlToyContext, SqlToyConfig sqlToyConfig, String sql,
+			Object[] paramsValue, Integer topSize, UpdateRowHandler updateRowHandler, Connection conn, Integer dbType,
+			String dialect) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public QueryResult updateFetchRandom(SqlToyContext sqlToyContext, SqlToyConfig sqlToyConfig, String sql,
+			Object[] paramsValue, Integer random, UpdateRowHandler updateRowHandler, Connection conn, Integer dbType,
+			String dialect) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StoreResult executeStore(SqlToyContext sqlToyContext, SqlToyConfig sqlToyConfig, String sql,
+			Object[] inParamsValue, Integer[] outParamsType, Connection conn, Integer dbType, String dialect)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
