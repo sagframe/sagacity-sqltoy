1452a4be853a0b1ce4bd33c7e3fa2720f68e93ac
zhongxuchen <zhongxuchen@gmail.com>
2022-06-09 12:33:29 +0800

    Update BeanUtil.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/utils/BeanUtil.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/utils/BeanUtil.java
@@ -476,7 +476,7 @@ public class BeanUtil {
 		// 非数组类型,但传递的参数值是数组类型且长度为1提取出数组中的单一值
 		if (!typeName.contains("[]") && paramValue.getClass().isArray()) {
 			if ((paramValue instanceof byte[]) && (typeName.equals("java.lang.string") || typeName.equals("string"))) {
-				paramValue = new String(((byte[]) paramValue));
+				paramValue = new String((byte[]) paramValue);
 			} else {
 				Object[] paramAry = CollectionUtil.convertArray(paramValue);
 				if (paramAry.length > 1) {
