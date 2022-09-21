f6fcf525b15fceba3aba12b89e0d8d22ff00a37c
zhongxuchen <zhongxuchen@gmail.com>
2022-09-21 23:20:53 +0800

    Update OperateType.java

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/config/model/OperateType.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/config/model/OperateType.java
@@ -18,7 +18,7 @@ public enum OperateType {
 	top(3),
 
 	random(4), count(5), load(6), loadAll(7), unique(8), fetchUpdate(9), execute(10), update(11), updateAll(12),
-	delete(13), deleteAll(13), saveOrUpdate(14);
+	delete(13), deleteAll(14), saveOrUpdate(15);
 
 	private final int optType;
 
