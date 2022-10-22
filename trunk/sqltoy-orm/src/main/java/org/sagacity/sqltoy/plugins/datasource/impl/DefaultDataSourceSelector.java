f16d0b45c9cfe631fcfedd1cba551b32f1711561
zhongxuchen <zhongxuchen@gmail.com>
2022-10-22 22:01:46 +0800

    更新

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/plugins/datasource/impl/DefaultDataSourceSelector.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/plugins/datasource/impl/DefaultDataSourceSelector.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.sagacity.sqltoy.exception.DataAccessException;
 import org.sagacity.sqltoy.integration.AppContext;
 import org.sagacity.sqltoy.plugins.datasource.DataSourceSelector;
 import org.sagacity.sqltoy.utils.StringUtil;
@@ -43,9 +44,18 @@ public class DefaultDataSourceSelector implements DataSourceSelector {
 		// 如果项目中只定义了唯一的数据源，则直接使用
 		if (result == null) {
 			Map<String, DataSource> dataSources = appContext.getBeansOfType(DataSource.class);
+			int dataSourceSize = dataSources.size();
 			// 只有一个dataSource,直接使用
-			if (dataSources.size() == 1) {
+			if (dataSourceSize == 1) {
 				result = dataSources.values().iterator().next();
+			} else {
+				if (dataSourceSize == 0) {
+					throw new DataAccessException("应用没有定义DataSource,请检查配置!");
+				}
+				throw new DataAccessException(
+						"应用中存在多个数据源，请指定当前线程对应的dataSource,\n1、可以通过spring.sqltoy.defaultDataSource=xxxx指定默认数据源!\n"
+								+ "2、如使用dynamic-dataSource类似插件,请配置primary=xxx 或@DS(\"xxxx\")\n"
+								+ "3、给dao注入具体的dataSource或通过方法传入具体dataSource!");
 			}
 		}
 		return result;
