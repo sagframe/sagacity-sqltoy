116ff7a8f2caf8fcbf8f42b483a13fd5e4cfa980
chenrenfei <zhongxuchen@gmail.com>
2023-02-24 09:52:47 +0800

    更新

--- trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/dialect/utils/ClickHouseDialectUtils.java
+++ trunk/sqltoy-orm/src/main/java/org/sagacity/sqltoy/dialect/utils/ClickHouseDialectUtils.java
@@ -9,6 +9,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -331,8 +332,113 @@ public class ClickHouseDialectUtils {
 		sqlToyResult = DialectUtils.doInterceptors(sqlToyContext, null, OperateType.update, sqlToyResult,
 				entity.getClass(), dbType);
 		Long updateCnt = SqlUtil.executeSql(sqlToyContext.getTypeHandler(), sqlToyResult.getSql(),
-				sqlToyResult.getParamsValue(), entityMeta.getFieldsTypeArray(), conn, dbType, null, false);
+				sqlToyResult.getParamsValue(), getIgnorePartionFieldsTypes(entityMeta), conn, dbType, null, false);
 		return updateCnt;
+	}
+
+	/**
+	 * @TODO 批量更新
+	 * @param sqlToyContext
+	 * @param entities
+	 * @param batchSize
+	 * @param forceUpdateFields
+	 * @param reflectPropsHandler
+	 * @param nullFunction
+	 * @param conn
+	 * @param dbType
+	 * @param autoCommit
+	 * @param tableName
+	 * @param skipNull
+	 * @return
+	 * @throws Exception
+	 */
+	public static Long updateAll(SqlToyContext sqlToyContext, List<?> entities, final int batchSize,
+			final String[] forceUpdateFields, ReflectPropsHandler reflectPropsHandler, String nullFunction,
+			Connection conn, final Integer dbType, final Boolean autoCommit, String tableName, boolean skipNull)
+			throws Exception {
+		if (entities == null || entities.isEmpty()) {
+			return 0L;
+		}
+		EntityMeta entityMeta = sqlToyContext.getEntityMeta(entities.get(0).getClass());
+		String realTable = entityMeta.getSchemaTable(tableName, dbType);
+		// 无主键
+		if (entityMeta.getIdArray() == null) {
+			throw new IllegalArgumentException("表:" + realTable + " 无主键,不符合update/updateAll规则,请检查表设计是否合理!");
+		}
+		// 全部是主键则无需update
+		if (entityMeta.getRejectIdFieldArray() == null) {
+			logger.warn("表:" + realTable + " 字段全部是主键不存在更新字段,无需执行更新操作!");
+			return 0L;
+		}
+		// 构造全新的修改记录参数赋值反射(覆盖之前的)
+		ReflectPropsHandler handler = DialectUtils.getUpdateReflectHandler(reflectPropsHandler, forceUpdateFields,
+				sqlToyContext.getUnifyFieldsHandler());
+		handler = DialectUtils.getSecureReflectHandler(handler, sqlToyContext.getFieldsSecureProvider(),
+				sqlToyContext.getDesensitizeProvider(), entityMeta.getSecureFields());
+		String[] fields = entityMeta.getFieldsNotPartitionKey();
+		List<Object[]> paramsValues = BeanUtil.reflectBeansToInnerAry(entities, fields, null, handler);
+		// 判断主键是否为空
+		int end = fields.length;
+		int pkIndex = end - entityMeta.getIdArray().length;
+		int index = 0;
+		// 累计多少行为空
+		int skipCount = 0;
+		Iterator<Object[]> iter = paramsValues.iterator();
+		Object[] rowValues;
+		while (iter.hasNext()) {
+			rowValues = iter.next();
+			for (int i = pkIndex; i < end; i++) {
+				// 判断主键值是否为空
+				if (StringUtil.isBlank(rowValues[i])) {
+					// 跳过主键值为空的
+					if (skipNull) {
+						skipCount++;
+						iter.remove();
+						break;
+					} else {
+						throw new IllegalArgumentException(
+								"通过对象对表" + realTable + " 进行updateAll操作,主键字段必须要赋值!第:" + index + " 条记录主键为null!");
+					}
+				}
+			}
+			index++;
+		}
+		if (skipCount > 0) {
+			logger.debug("共有:{}行记录因为主键值为空跳过修改操作!", skipCount);
+		}
+		// 构建update语句
+		String updateSql = generateUpdateSql(dbType, entityMeta, nullFunction, forceUpdateFields, realTable);
+		if (updateSql == null) {
+			throw new IllegalArgumentException("updateAll sql is null,引起问题的原因是没有设置需要修改的字段!");
+		}
+		SqlToyResult sqlToyResult = new SqlToyResult(updateSql, null);
+		// 增加sql执行拦截器 update 2022-9-10
+		sqlToyResult = DialectUtils.doInterceptors(sqlToyContext, null, OperateType.updateAll, sqlToyResult,
+				entities.get(0).getClass(), dbType);
+		SqlExecuteStat.showSql("批量修改[" + paramsValues.size() + "]条记录", sqlToyResult.getSql(), null);
+		return SqlUtilsExt.batchUpdateForPOJO(sqlToyContext.getTypeHandler(), sqlToyResult.getSql(), paramsValues,
+				getIgnorePartionFieldsTypes(entityMeta), null, null, batchSize, autoCommit, conn, dbType);
+	}
+
+	/**
+	 * @TODO 获取排除分区字段的所有字段对应的类型
+	 * @param entityMeta
+	 * @return
+	 */
+	private static Integer[] getIgnorePartionFieldsTypes(EntityMeta entityMeta) {
+		List<Integer> fieldTypes = new ArrayList<Integer>();
+		FieldMeta fieldMeta;
+		String[] fields = entityMeta.getFieldsArray();
+		Integer[] fieldTypesArray = entityMeta.getFieldsTypeArray();
+		for (int i = 0; i < fields.length; i++) {
+			fieldMeta = entityMeta.getFieldMeta(fields[i]);
+			if (!fieldMeta.isPartitionKey()) {
+				fieldTypes.add(fieldTypesArray[i]);
+			}
+		}
+		Integer[] result = new Integer[fieldTypes.size()];
+		fieldTypes.toArray(result);
+		return result;
 	}
 
 	private static String generateUpdateSql(Integer dbType, EntityMeta entityMeta, String nullFunction,
