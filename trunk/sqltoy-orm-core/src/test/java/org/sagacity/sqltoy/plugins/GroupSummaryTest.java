d512b2d33b0c71212232ae0fe3ec0e1a018b2fab
zhongxuchen <zhongxuchen@gmail.com>
2024-07-04 22:53:21 +0800

    更新

--- trunk/sqltoy-orm-core/src/test/java/org/sagacity/sqltoy/plugins/GroupSummaryTest.java
+++ trunk/sqltoy-orm-core/src/test/java/org/sagacity/sqltoy/plugins/GroupSummaryTest.java
@@ -13,7 +13,7 @@ import org.sagacity.sqltoy.utils.ExpressionUtil;
 import org.sagacity.sqltoy.utils.NumberUtil;
 import org.sagacity.sqltoy.utils.StringUtil;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 
 public class GroupSummaryTest {
 	@Test
