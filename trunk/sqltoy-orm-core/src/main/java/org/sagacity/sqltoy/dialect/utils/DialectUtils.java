9dc0401dca38f3fb80e14ef93cbcf6629e6b9449
zhongxuchen <zhongxuchen@gmail.com>
2024-10-26 23:12:54 +0800

    Update DialectUtils.java

--- trunk/sqltoy-orm-core/src/main/java/org/sagacity/sqltoy/dialect/utils/DialectUtils.java
+++ trunk/sqltoy-orm-core/src/main/java/org/sagacity/sqltoy/dialect/utils/DialectUtils.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -1673,8 +1674,9 @@ public class DialectUtils {
 		final Integer[] paramsType = entityMeta.getFieldsTypeArray();
 		PreparedStatement pst = null;
 		if (isIdentity || isSequence) {
-			pst = conn.prepareStatement(realInsertSql, new String[] { DataSourceUtils
-					.getReturnPrimaryKeyColumn(entityMeta.getColumnName(entityMeta.getIdArray()[0]), dbType) });
+//			pst = conn.prepareStatement(realInsertSql, new String[] { DataSourceUtils
+//					.getReturnPrimaryKeyColumn(entityMeta.getColumnName(entityMeta.getIdArray()[0]), dbType) });
+			pst = conn.prepareStatement(realInsertSql, Statement.RETURN_GENERATED_KEYS);
 		} else {
 			pst = conn.prepareStatement(realInsertSql);
 		}
