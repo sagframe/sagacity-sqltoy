564f66431140248855421710f65ccc0f0d7ec57e
chenrenfei@sinochem.com <chenrenfei@sinochem.com>
2026-05-15 10:26:07 +0800

    更新

--- trunk/sqltoy-orm-core/src/test/java/org/sagacity/sqltoy/plugins/DDLTest.java
+++ trunk/sqltoy-orm-core/src/test/java/org/sagacity/sqltoy/plugins/DDLTest.java
@@ -17,8 +17,17 @@ import org.sagacity.sqltoy.utils.DataSourceUtils.DBType;
 public class DDLTest {
 	@Test
 	public void testCreateSqlFile() {
+		// 指定POJO所在的包路径
 		String[] scanPackages = new String[] { "org.sagacity.sqltoy.demo.domain" };
 		try {
+			/**
+			 * @param scanPackages
+			 * @param saveFile            脚本存放文件
+			 * @param upperOrLower        upper|lower 脚本表名、字段名是否统一转大写或小写
+			 * @param dbType              数据库类型，用DBType.xx 提供
+			 * @param schema              针对sqlserver需要提供(其他数据库可为null)，项目启动时会根据connection获取schema
+			 * @param dialectDDLGenerator 自己指定ddl创建器,如果是：mysql、oracle、pg、sqlserver等数据库无需扩展，传递null即可
+			 */
 			DDLFactory.createSqlFile(scanPackages, "D://sqltoy.sql", "upper", DBType.MYSQL, null, null);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
